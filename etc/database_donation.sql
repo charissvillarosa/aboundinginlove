cf085ab0aa8bbc376fb83215a1b92bf53ed5f424
Chariss <Chariss@Chariss-PC>
2013-05-02 20:04:07 +0800

    added database structure

---
+++ etc/database_donation.sql
@@ -0,0 +1,102 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.15 
+MySQL - 5.1.41 : Database - donation
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`donation` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `donation`;
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sponsee_images` */
+
+DROP TABLE IF EXISTS `sponsee_images`;
+
+CREATE TABLE `sponsee_images` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `sponsee_id` bigint(20) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `date_uploaded` datetime DEFAULT NULL,
+  `image` blob,
+  `content_type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sponsee_needs` */
+
+DROP TABLE IF EXISTS `sponsee_needs`;
+
+CREATE TABLE `sponsee_needs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `added_by` varchar(100) DEFAULT NULL,
+  `date_added` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sponsee_needs_category` */
+
+DROP TABLE IF EXISTS `sponsee_needs_category`;
+
+CREATE TABLE `sponsee_needs_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sponsees` */
+
+DROP TABLE IF EXISTS `sponsees`;
+
+CREATE TABLE `sponsees` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `middlename` varchar(100) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `country` varchar(5) DEFAULT NULL,
+  `maplocation` varchar(500) DEFAULT NULL,
+  `information` text,
+  `birthdate` date DEFAULT NULL,
+  `primaryimage` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
