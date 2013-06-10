fcbcaae16f9f11691550482a6cc942ec2dfd0574
jayrome vergara <jaycverg@gmail.com>
2013-06-10 21:38:39 +0800

    updated paypal_txn_logs.sql scripts

--- etc/paypal_txn_logs.sql
+++ etc/paypal_txn_logs.sql
@@ -1,29 +1,30 @@
-create table paypal_txn_log (
-	id        varchar(20) primary key,
-	refno         varchar(150),
-	first_name    varchar(64),
-	last_name     varchar(64),
-	payer_email   varchar(127),
-	payer_id      varchar(13),
-	payer_status  varchar(20),
-	contact_phone varchar(20),
-	payment_fee   decimal(10,2),
-	payment_gross decimal(10,2),
-	amount decimal(10,2)
-);
+CREATE TABLE `paypal_txn_logs` (
+    `id` varchar(20) NOT NULL,
+    `refno` varchar(150) DEFAULT NULL,
+    `first_name` varchar(64) DEFAULT NULL,
+    `last_name` varchar(64) DEFAULT NULL,
+    `payer_email` varchar(127) DEFAULT NULL,
+    `payer_id` varchar(13) DEFAULT NULL,
+    `payer_status` varchar(20) DEFAULT NULL,
+    `contact_phone` varchar(20) DEFAULT NULL,
+    `payment_fee` decimal(10,2) DEFAULT NULL,
+    `payment_gross` decimal(10,2) DEFAULT NULL,
+    `amount` decimal(10,2) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 
 CREATE TABLE `paypal_items` (
-  `id` varchar(36) NOT NULL,
-  `instant_payment_notification_id` varchar(36) NOT NULL,
-  `item_name` varchar(127) default NULL,
-  `item_number` varchar(127) default NULL,
-  `quantity` varchar(127) default NULL,
-  `mc_gross` float(10,2) default NULL,
-  `mc_shipping` float(10,2) default NULL,
-  `mc_handling` float(10,2) default NULL,
-  `tax` float(10,2) default NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
+    `id` varchar(36) NOT NULL,
+    `instant_payment_notification_id` varchar(36) NOT NULL,
+    `item_name` varchar(127) default NULL,
+    `item_number` varchar(127) default NULL,
+    `quantity` varchar(127) default NULL,
+    `mc_gross` float(10,2) default NULL,
+    `mc_shipping` float(10,2) default NULL,
+    `mc_handling` float(10,2) default NULL,
+    `tax` float(10,2) default NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
